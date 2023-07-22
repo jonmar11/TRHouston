a0df398ae0c70d073834959b366597f4c7492f8a
jonmar11 <j.alec.martinez@gmail.com>
2023-07-22 14:28:03 -0500

    massive updates, footer started, images imported

--- TEAMFK8/website.js
+++ TEAMFK8/website.js
@@ -1,4 +1,6 @@
 // taking user back to top (need to create button later)
 const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
 
-//
+// dates
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
